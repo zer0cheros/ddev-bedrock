f2200772fb2a023dfa91eb2ed520ed4c09bcd2cd
Zer0cheros <christian.wiss@optimaedu.fi>
2023-03-21 18:16:53 +0200

    commit

---
+++ web/app/themes/wp-zer0cheros/src/componets/PageComponet/PageComponet.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Page from '@/types/types'
+
+
+const PageComponet:React.FC<{pageProps: Page[]}> = ({pageProps}) => {
+    return (
+    <div>
+        {pageProps.map(page=>(
+            <div key={page.id}>
+                <h1>{page.title.rendered}</h1>
+            </div>
+            
+        ))}
+    </div>
+  )
+}
+
+export default PageComponet
